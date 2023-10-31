2002f078644d189ae3de24136416b1bbf18b8d51
Sibongile <sibongile.nhlema@gmail.com>
2023-10-31 12:04:30 -0700

    task 5 completed: removed extra 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
-    print("{:02d}, ".format(numbers), end="")
     if numbers == 99:
         print("{:02d}".format(numbers))
+        break
+    print("{:02d}, ".format(numbers), end="")
