307e7b36e40eb14366dca7e82dced478cbfefbda
Sibongile <sibongile.nhlema@gmail.com>
2023-10-31 15:52:03 -0700

    fixed pycodestyle issues

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,6 @@
 def remove_char_at(str, n):
     copy_of_str = str
     if n >= len(str) or n < 0:
-        return(str)
+        return (str)
     copy_of_str = str[:n] + str[n+1:]
-    return(copy_of_str)
+    return (copy_of_str)
