872d1c5317fcbe4389054d2318f484dbf0eb3adc
Sibongile <sibongile.nhlema@gmail.com>
2023-10-31 16:25:21 -0700

    changed condition

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -5,6 +5,7 @@ import dis
 def magic_calculation(a, b, c):
     if not (a < b):
         return (c)
-    if not (c > b):
+    elif not (c > b):
         return (a + b)
-    return (a * b - c)
+    else:
+        return (a * b - c)
