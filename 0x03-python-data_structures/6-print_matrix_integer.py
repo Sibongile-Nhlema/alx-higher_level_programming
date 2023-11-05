cfadeca185634809124dca722e3a994ae9ba4a98
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 04:08:35 -0800

    redid task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix[0])):
-        for j in range(0, len(matrix[1])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+    for row in matrix:
+        for i in row:
+            print(" ".join('{:d}'.format(i)), end='')
         print()
