53e03fcc56032a4f1f756819b01c07b08da5ce67
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 03:21:11 -0800

    changed return value of first condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0 or idx > len(my_list):
-        return (list_copy)
+        return (my_list)
     else:
         list_copy[idx] = element
         return (list_copy)
