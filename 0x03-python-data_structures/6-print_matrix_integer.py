bd12f88ac935d35c60f88fc026f0ebd8c0f1060c
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 04:13:19 -0800

    added spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print(" ".join('{:d}'.format(i)), end='')
+            print(" ".join("{:d}".format(i)), end=" ")
         print()
