c53f145e0023d7223e334398a3d08eda0a3d16ba
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 04:16:20 -0800

    added condition to print statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print(" ".join("{:d}".format(i)), end=" ")
-        print()
+        print(" ".join("{:d}".format(i)for i in row))
