1d2ea224d332ad3756d4e78653c358395c4b06ca
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 05:34:57 -0800

    rewrote task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0]
     if sentence is None:
-        return (len(sentence), "None")
-    return (len(sentence), sentence[0])
+        first_char = "None"
+    my_tuple = (length, first_char)
+    return (my_tuple)
