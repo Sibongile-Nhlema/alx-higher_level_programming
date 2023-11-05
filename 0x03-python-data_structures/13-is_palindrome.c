16a7ce76f677f180b7267737a59e00720b59bb47
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 09:59:38 -0800

    refactors repetitive code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,8 +10,7 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *copy_head;
-	listint_t *first, *last, *current;
+	listint_t *copy_head, *first, *last, *current;
 	/*make a copy of the head*/
 	copy_head = *head;
 	/*main loop check if first and are equal*/
@@ -20,34 +19,19 @@ int is_palindrome(listint_t **head)
 		/*find first elemnet*/
 		first = copy_head;
 		/*find last element*/
-		last = copy_head;
 		current = copy_head;
 		while (current->next != NULL)
 		{
+			last = current;
 			current = current->next;
 		}
-		last = current;
 		/*compare element*/
-		if (first->n == last->n)
-		{
-		/*update first element to second element*/
-			first = first->next;
-		/*update last element to second last element*/
-			current = copy_head;
-			while (current->next->next != NULL)
-			{
-				current = current->next;
-			}
-			last = current;
-			last->next = NULL;
-			copy_head = copy_head->next;
-		/*making a secons last node point to NULL*/
-		}
-		else
+		if (first->n != last->n)
 		{
 			return (0);
 		}
-	/*if not break from loop and return 0*/
+		last->next = NULL;
+		copy_head = first->next;
 	}
 	return (1);
 }
