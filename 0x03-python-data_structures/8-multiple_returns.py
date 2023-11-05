dd9a546451de276920c6c878c2d613c228c669e1
Sibongile <sibongile.nhlema@gmail.com>
2023-11-05 05:39:00 -0800

    changed conditions

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0]
-    if sentence is None:
-        first_char = "None"
-    my_tuple = (length, first_char)
-    return (my_tuple)
+    if not sentence:
+        return (len(sentence), "None")
+    return (len(sentence), sentence[0])
