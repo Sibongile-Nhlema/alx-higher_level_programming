c421cd269be9b828caf51a7a833e61bcf946a0b6
Sibongile <sibongile.nhlema@gmail.com>
2023-11-08 09:12:36 -0800

    redid task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print(key, ":", value)
+
+    """
     if not a_dictionary:
         return
     for key in map(lambda x: x[0], sorted(a_dictionary.items())):
         print("{} : {}".format(key, a_dictionary[key]))
+    """
