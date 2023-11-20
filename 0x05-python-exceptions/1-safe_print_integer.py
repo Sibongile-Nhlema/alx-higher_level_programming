c5e11fb8a8b45f3a6cdeda4775b1c69a02f1f2ff
Sibongile <sibongile.nhlema@gmail.com>
2023-11-20 06:39:15 -0800

    added more exepctions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    if value is None:
+        return False
     try:
         if isinstance(value, int):
             print("{:d}".format(value))
             return True
-    except TypeError:
+    except (TypeError, ValueError):
         return False
