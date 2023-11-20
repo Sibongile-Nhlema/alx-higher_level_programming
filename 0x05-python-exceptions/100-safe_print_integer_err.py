162d717972df4938182150236eeaf94db4c231cb
Sibongile <sibongile.nhlema@gmail.com>
2023-11-20 09:17:34 -0800

    used as instead

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import sys
 def safe_print_integer_err(value):
-    if value is None:
-        return False
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr))
+    except (TypeError, ValueError) as e:
+        print("Exception: {}".format(str(e)), file=sys.stderr)
         return False
