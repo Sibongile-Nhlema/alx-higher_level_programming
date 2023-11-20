9cd616445705ce312921d37eec52c648dc0cceb6
Sibongile <sibongile.nhlema@gmail.com>
2023-11-20 08:11:53 -0800

    removed spaces

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for x in range(0, list_length):
         try:
-            new_element = my_list_1[x] / my_list_2[x]
+            new_element = my_list_1[x]/my_list_2[x]
         except TypeError:
             print("wrong type")
             new_element = 0
