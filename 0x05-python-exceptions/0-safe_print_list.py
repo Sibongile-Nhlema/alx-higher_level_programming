2fd5d82d98450ea973e5d171e01e1b2ab1bd1f62
Sibongile <sibongile.nhlema@gmail.com>
2023-11-20 06:14:51 -0800

    fixed pycodestyle errors in task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,15 +11,6 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end="")
                 num_of_elements = i + 1
         print()
-    except:
+    except TypeError:
         pass
     return num_of_elements
-        
-'''
-   keep track of number of elements 
-   try-except block to handle potential errors
-   Iterate over the elements
-   check if counter == x, if so, break
-   print element and increment counter
-   return counter
-'''
