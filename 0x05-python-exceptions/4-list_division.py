d5510496a787c0a9e1453771f1389016a940c0a6
Sibongile <sibongile.nhlema@gmail.com>
2023-11-20 07:55:39 -0800

    changed order of exceptions to match requirements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,11 +4,11 @@ def list_division(my_list_1, my_list_2, list_length):
     for x in range(0, list_length):
         try:
             new_element = my_list_1[x] / my_list_2[x]
-        except ZeroDivisionError:
-            print("division by zero")
-            new_element = 0
         except TypeError:
             print("wrong type")
+            new_element = 0
+        except ZeroDivisionError:
+            print("division by zero")
             new_element = 0
         except IndexError:
             print("out of range")
