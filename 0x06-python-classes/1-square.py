dffc512303d38507ca26ea7bc4e226faf98d7dec
Sibongile <sibongile.nhlema@gmail.com>
2023-11-21 08:05:23 -0800

    changed doc string

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-''' Module for the Square definition'''
+''' Module for the Square definition.'''
 
 
 class Square:
-    ''' Defines a new square '''
+    ''' Defines a new square. '''
     def __init__(self, size):
         ''' Constructor.
         Args:
-            size: width and height of the Square
+            size: width and height of the Square.
         '''
         self.___size = size
