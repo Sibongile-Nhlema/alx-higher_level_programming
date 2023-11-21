56cd0ab77104ce9dc07662710d50d0e1df1fabf6
Sibongile <sibongile.nhlema@gmail.com>
2023-11-21 17:56:01 +0200

    created task 1

---
+++ 0x06-python-classes/1.square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Module for the Square definition'''
+
+
+class Square:
+    ''' Defines a new square '''
+    def __init__(self, size):
+        ''' Constructor with private size.
+        Arguments:
+            size: width and height of the Square
+        '''
+        self.___size = size
