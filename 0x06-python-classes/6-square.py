fcd176741a34e58dfaece5b3b15fd27d1787b0ac
Sibongile <sibongile.nhlema@gmail.com>
2023-11-22 03:51:34 -0800

    fixed some errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,9 +75,12 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            if self.__position[0] < 2 and self.__position[0] > 0:
+                print()
             for i in range(self.__size):
                 for k in range(0, self.__position[0]):
                     print(" ", end="")
                 for j in range(1, self.__size):
                     print("#", end="")
-                print("#")
+                print("#", end="")
+                print()
