6c2d5ccf5b811d5365af0eecec639448a69732c4
Sibongile <sibongile.nhlema@gmail.com>
2023-11-27 01:54:03 -0800

    changed conditions so that they use value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,9 +22,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         ''' This setes the height to the new vlaue with some exceptions'''
-        if not isinstance(self.__height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -36,8 +36,8 @@ class Rectangle:
     @width.setter
     def width(self, value):
         ''' This sets the width to the new value with some exceptions'''
-        if not isinstance(self.__width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
