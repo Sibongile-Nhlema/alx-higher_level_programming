5a5692a36e17139d654d1ac4317fcb04f50eeb4b
Sibongile <sibongile.nhlema@gmail.com>
2023-11-27 07:00:22 -0800

    removed docstring

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -107,7 +107,6 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         ''' Creates a new rectangle with this instance
-            width == height == size
             Args:
                 size (int): new dimensions
             Return: new Rectangle instance with width == height == size
