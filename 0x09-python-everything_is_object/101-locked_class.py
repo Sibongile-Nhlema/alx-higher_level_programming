869fef0b796c6b2e647d8aad7922546850e75ab1
Sibongile <sibongile.nhlema@gmail.com>
2023-11-28 06:42:59 -0800

    fixed silly mistake

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,5 +6,5 @@ class LockedClass:
     ''' Prevents user fomr dynamically creating new attr.
         unless that new attr is "first_name"
     '''
-    ___slots___ = ["first_name"]
+    __slots__ = ["first_name"]
     ''' only allowed attribute '''
