01f5d26e30ee1e6da64902bf82f75ecd58250438
Sibongile <sibongile.nhlema@gmail.com>
2023-12-04 08:53:45 -0800

    completed task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' Module for MyInt class  '''
+
+
+class MyInt(int):
+    ''' inherits from int
+        Args:
+        int(int): class
+    '''
+    def __bool__(int):
+        return False
