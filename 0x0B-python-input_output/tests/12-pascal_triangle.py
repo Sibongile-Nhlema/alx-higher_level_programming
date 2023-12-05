df5b3f3cf40a7f80cbca72d0212b6ebb6135fb43
Sibongile <sibongile.nhlema@gmail.com>
2023-12-05 10:44:46 -0800

    completed task 12

---
+++ 0x0B-python-input_output/tests/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' defines pascal_triangle '''
+
+
+def pascal_triangle(n):
+    ''' defines a triangle
+        Args:
+            n(int): integer
+        Return: list of lists of integers representing
+            Pascal's triangle of n
+    '''
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+    #return triangle
+    #check if negative
+    #n = number of rows
+    #print 1, then print sum of index 0 and 1
+    #print it in the middle
