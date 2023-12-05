0a5cdb61d0c9e8a31bd7122b3b02c8cda2fbca72
Sibongile <sibongile.nhlema@gmail.com>
2023-12-05 06:07:02 -0800

    fixed some silly mistakes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     '''
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
-    print("{}".format(read_file))
+        print("{}".format(read_file), end="")
