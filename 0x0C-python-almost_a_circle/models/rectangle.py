aafc346b52cadaed2c41ef5be907dfb3e99e4bb7
Sibongile <sibongile.nhlema@gmail.com>
2023-12-08 11:23:33 +0000

    used conventions given in examples

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,60 +5,59 @@ from models.base import Base
 
 class Rectangle(Base):
     ''' Defines a retangle by inheriting from the Base class
-        Arsg:
+        Args:
             Base: class it inherits from
-    ''' 
+    '''
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' initialises a rectangle instance
             Args:
                 width(int): width of the rectangle
                 height(int): height of the rectangle
-                x(int): unknown
-                y(int): unknown
+                x(int): x-coordinates of the rectangle
+                y(int): y-coordinates of the rectangle
         '''
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
-    def set_width(self):
-        ''' gets the width attr '''
-        return self.__width
+    def width(self):
+       ''' gets the width attr '''
+       return self.__width
 
-    @set_width.setter
-    def set_width(self, value):
+    @width.setter
+    def width(self, value):
         ''' sets the width attr '''
         self.__width = value
 
     @property
-    def set_height(self):
-        ''' gets the height attr '''
-        return self.__height
+    def height(self):
+       ''' gets the height attr '''
+       return self.__height
 
-    @set_height.setter
-    def set_height(self, value):
+    @height.setter
+    def height(self, value):
         ''' sets the height attr '''
         self.__height = value
 
     @property
-    def set_x(self):
+    def x(self):
         ''' gets the x attr '''
         return self.__x
 
-    @set_x.setter
-    def set_x(self, value):
-        ''' sets the x attr '''
-        self.__x = value
+    @x.setter
+    def x(self, value):
+       ''' sets the x attr '''
+       self.__x = value
 
     @property
-    def set_y(self):
+    def y(self):
         ''' gets the y attr '''
         return self.__y
 
-    @set_y.setter
-    def set_y(self):
+    @y.setter
+    def y(self, value):
         ''' sets the y attr '''
         self.__y = value
-
