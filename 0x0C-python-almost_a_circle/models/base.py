187b34a26f1fa283e5e9c137b160e846e8748019
Sibongile <sibongile.nhlema@gmail.com>
2023-12-10 07:37:01 -0800

    completed task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,10 +22,28 @@ class Base:
     def to_json_string(list_dictionaries):
         ''' returns the JSON string representation of list_dictionaries
             Args:
-                list_dictionaries: list of dicts
+                list_dictionaries(dict): list of dicts
             Return: None if empty or json string
         '''
         if list_dictionaries is not None or list_dictionaries != []:
             return json.dumps(list_dictionaries)
         else:
             return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' writes JSON string representation of list_objs to file
+            Args:
+                cls(obj): class
+                list_objects(list): list of instances who inherits Base
+        '''
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write([])
+            else:
+                # to_json_string taks dict as a paramater
+                list_of_dicts = []
+                for i in list_objs:
+                    list_of_dicts.append(i.to_dictionary())
+                f.write(Base.to_json_string(list_of_dicts))
