055106744790bba9ee635bac11ae533aa89ba759
Sibongile <sibongile.nhlema@gmail.com>
2023-12-11 01:52:25 -0800

    added json to string test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,7 +9,7 @@ import unittest
 from models.base import Base
 
 
-class ModelBaseTest(unittest.TestCase):
+class TestBase_init(unittest.TestCase):
     ''' Defines test cases for the base class '''
 
 #  no id's given
@@ -120,6 +120,57 @@ class ModelBaseTest(unittest.TestCase):
 
     def test_id_memoryview(self):
         self.assertEqual(memoryview(bytes(10)), Base(memoryview(bytes(10))).id)
+
+
+class TestBase_to_json_string(unittest.TestCase):
+    ''' Test for to_json_string '''
+    def test_to_json_string_no_arg(self):
+        with self.assertRaises(TypeError):
+            Base.to_json_string()
+
+    def test_to_json_string_none(self):
+        self.assertEqual("[]", Base.to_json_string(None))
+
+    def test_to_json_string_empty(self):
+        self.assertEqual("[]", Base.to_json_string([]))
+
+    def test_to_json_string_more_than_one_arg(self):
+        with self.assertRaises(TypeError):
+            Base.to_json_string([], 1)
+
+
+class TestBase_to_json_string_rectangle(unittest.TestCase):
+    ''' Test for to_json_string '''
+    def test_to_json_string_type_rectangle(self):
+        rec1 = Rectangle(3, 5, 7, 9, 11)
+        self.assertEqual(str, type(Base.to_json_string([rec1.to_dictionary()])))
+
+    def test_to_json_string_rectangle_one_dict(self):
+        r = Rectangle(10, 7, 2, 8, 6)
+        self.assertTrue(len(Base.to_json_string([r.to_dictionary()])) == 53)
+
+    def test_to_json_string_rectangle_two_dicts(self):
+        r1 = Rectangle(2, 3, 5, 19, 2)
+        r2 = Rectangle(4, 2, 4, 1, 12)
+        list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
+        self.assertTrue(len(Base.to_json_string(list_dicts)) == 106)
+
+
+class TestBase_to_json_string_square(unittest.TestCase):
+    ''' Test for to_json_string '''
+    def test_to_json_string_type_square(self):
+        s = Square(5, 20, 15, 4)
+        self.assertEqual(str, type(Base.to_json_string([s.to_dictionary()])))
+
+    def test_to_json_string_square_one_dict(self):
+        s = Square(10, 2, 3, 4)
+        self.assertTrue(len(Base.to_json_string([s.to_dictionary()])) == 39)
+
+    def test_to_json_string_square_two_dicts(self):
+        s1 = Square(10, 2, 3, 4)
+        s2 = Square(4, 5, 21, 2)
+        list_dicts = [s1.to_dictionary(), s2.to_dictionary()]
+        self.assertTrue(len(Base.to_json_string(list_dicts)) == 78)
 
 
 if __name__ == "__main__":
