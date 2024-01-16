828dfe087acc1b23cb8fb63be552f340a00a29ff
Sibongile <sibongile.nhlema@gmail.com>
2024-01-16 12:12:22 -0800

    added a where condition to handle the name requirement

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- This script lists all records of the table second_table with a name value in DESC order
-SELECT score, `name` FROM `second_table` ORDER BY `score` DESC
+SELECT score, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC
