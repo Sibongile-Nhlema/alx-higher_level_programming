18bb814772b66e78e69aded44fa1fec3cb4e4175
Sibongile <sibongile.nhlema@gmail.com>
2024-01-17 09:25:48 -0800

    completed task 2: added a new database and user with select privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- This script creates a database and a new user
+-- The new user will only have SELECT privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* to 'user_0d_2'@'localhost'
+WITH GRANT OPTION;
