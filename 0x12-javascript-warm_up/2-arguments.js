cebeb0610158aee9f98f7b1554cb54a7b27f7abe
Sibongile <sibongile.nhlema@gmail.com>
2024-02-13 16:33:51 +0000

    fixed typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
-  console.log('Agrument found');
+  console.log('Argument found');
 } else {
-  console.log('Agruments found');
+  console.log('Arguments found');
 }
