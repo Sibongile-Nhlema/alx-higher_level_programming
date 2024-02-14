6a11858cf87020f3a644c60c477fb596e056912f
Sibongile <sibongile.nhlema@gmail.com>
2024-02-14 16:08:49 +0000

    fixed semistandard errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,17 +3,17 @@
 const Square1 = require('./5-square');
 
 class Square extends Square1 {
-	charPrint (c) {
-		if (c === undefined) {
-			c = 'X';
-		}
-		for (let i = 0; i < this.height; i++) {
-			let rect = '';
-			for (let j = 0; j < this.width; j++) {
-				rect += c;
-			}
-			console.log(rect);
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let j = 0; j < this.width; j++) {
+        rect += c;
+      }
+      console.log(rect);
+    }
+  }
 }
 module.exports = Square;
