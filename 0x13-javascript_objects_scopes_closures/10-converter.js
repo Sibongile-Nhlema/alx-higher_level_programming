6ef917d547ba7045edcd018377af35794762b104
Sibongile <sibongile.nhlema@gmail.com>
2024-02-14 16:54:37 +0000

    fixed the semistandard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-	return function (digit) {
-		return digit.toString(base);
-	};
-}
+  return function (digit) {
+    return digit.toString(base);
+  };
+};
