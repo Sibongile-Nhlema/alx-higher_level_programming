9a85a497b9bc254507bed608d2537ae4b8988b15
Sibongile <sibongile.nhlema@gmail.com>
2024-02-14 15:40:51 +0000

    fixed the rotate method

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,13 +20,13 @@ class Rectangle {
 
   rotate () {
     const temp = this.width;
-    this.height = this.width;
+    this.width = this.height;
     this.height = temp;
   }
 
   double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
+    this.height *= 2;
+    this.width *= 2;
   }
 }
 module.exports = Rectangle;
