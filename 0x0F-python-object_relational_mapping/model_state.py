4ad7e7293ef96be04b62a1bf2407d042e5811991
Sibongile <sibongile.nhlema@gmail.com>
2024-02-16 19:25:30 +0000

    fixed pycodestyle errors

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,5 +11,6 @@ Base = declarative_base(metadata=ourMetaData)
 class State(Base):
     '''Class with id and name attributes '''
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True,
+            autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
