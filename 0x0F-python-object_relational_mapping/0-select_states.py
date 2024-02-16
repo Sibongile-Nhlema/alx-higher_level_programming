4921f70b445ed29cfe9e75c3bf12660584a6716d
Sibongile <sibongile.nhlema@gmail.com>
2024-02-16 09:26:30 +0000

    completed task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''script that lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    '''Connect to the MySQL server'''
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+
+    '''Execute the SQL query to retrive the states'''
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    '''Show results'''
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
