e60c864483a5b80f1a9052a6b691746e087470be
Sibongile <sibongile.nhlema@gmail.com>
2024-02-16 18:55:48 +0000

    completed task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''State and base classes - instance of declarative_base()'''
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
