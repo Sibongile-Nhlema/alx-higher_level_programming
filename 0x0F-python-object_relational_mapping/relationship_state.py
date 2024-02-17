20d33139c1a2c74489985900591cd7583f6cf976
Your Name <sibongile.nhlema@gmail.com>
2024-02-17 03:06:39 -0800

    completed task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' IMPROVED - State and base classes - instance of declarative_base()'''
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+ourMetaData = MetaData()
+Base = declarative_base(metadata=ourMetaData)
+
+
+class State(Base):
+    '''Class with id and name attributes '''
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
