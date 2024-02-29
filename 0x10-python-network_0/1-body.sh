53c238fc5db53b5cddea84ca4a66cd93a94a73e7
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 22:41:06 +0200

    undid prevoius changes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script takes URL, sends a GET request, displays body of response for 200 status code
-curl -sL "$1" | grep -q "200" && curl -sL "$1"
+# script takes URL, sends a GET request, displays body of response for 200 status cod
+curl -sL -w "%{http_code}" -o /dev/null "$1" -X GET | grep -q "200" && curl -sL "$1"
