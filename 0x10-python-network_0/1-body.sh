7b1ce4cc792375b5b0586415319a26b33683eada
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 21:42:18 +0200

    redid task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response for a 200 status code
-curl -s "$1" -X GET -D - -o /dev/null
+# This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
+curl -sL -w "%{http_code}" -o /dev/null "$1" -X GET
+
