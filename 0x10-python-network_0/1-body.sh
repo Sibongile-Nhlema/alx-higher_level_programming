496c199c0a781847d864c44adfdd9d16c7307bac
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 22:38:13 +0200

    refined the code to not print the https status as well

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes URL, sends a GET request, displays body of response for 200 status code
-curl -sL -w "%{http_code}" -o /dev/null "$1" -X GET && curl -sL "$1"
+curl -sL "$1" | grep -q "200" && curl -sL "$1"
