46b317dc6267137178dee09b8b93545f3c4a539e
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 21:44:15 +0200

    reduced the number of lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
+# script takes URL, sends a GET request, displays body of response for 200 status code
 curl -sL -w "%{http_code}" -o /dev/null "$1" -X GET
-
