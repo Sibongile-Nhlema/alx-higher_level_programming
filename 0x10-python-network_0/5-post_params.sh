d621e2a5df73289b22963a0447c57f97cf28bd97
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 23:29:43 +0200

    removed extra line

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request with specific parameters, and displays body
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
