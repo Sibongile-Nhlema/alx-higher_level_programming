afcdcc16b2a0d5624f03b798dd4f28b68b399d2f
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 22:58:37 +0200

    removed excessive code

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends DELETE request to URL passed as irst argument, displays body ofresponse
-curl -sL -w "%{http_code}" -o /dev/null "$1" -X DELETE
+curl -sL -w "%{http_code}" "$1" -X DELETE
