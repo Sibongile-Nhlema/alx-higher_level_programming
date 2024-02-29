211bfcbccdd726451e68b49f0d7b103f1ca62ca9
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 22:48:55 +0200

    script sends a delete request and displays the body of the response

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a curl request, and displays the size of the response body in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# sends DELETE request to URL passed as irst argument, displays body ofresponse
+curl -sL -w "%{http_code}" -o /dev/null "$1" -X DELETE
