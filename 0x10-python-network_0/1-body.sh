d23bd2d3c6b3eb739fb22f8d7d35273b09f7f6ed
Sibongile <sibongile.nhlema@gmail.com>
2024-02-29 22:18:13 +0200

    redid task 1 with a different condition

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes URL, sends a GET request, displays body of response for 200 status code
-curl -sL -w "%{http_code}" -o /dev/null "$1" -X GET
+curl -sL -w "%{http_code}" -o /dev/null "$1" -X GET && curl -sL "$1"
