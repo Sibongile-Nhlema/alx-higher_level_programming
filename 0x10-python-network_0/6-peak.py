eb1f1fe3965b35cbb980694261d0279aba69aa84
Sibongile <sibongile.nhlema@gmail.com>
2024-03-02 21:05:31 +0200

    started doing the interview questions

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+	''' Function finds the peak in a list of unordered integers '''
+        for i in range(list_of_integers):
+		print(list_of_integers[i])	
+	return list_of_integers
