e888c6345ad3d21d7c5db9ecefd24dedc83ee265
Sibongile <sibongile.nhlema@gmail.com>
2024-03-03 08:18:07 -0800

    removed edge case handling

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,10 +6,6 @@ import urllib.request
 import sys
 
 
-if len(sys.argv) < 2:
-    sys.exit(1)
-
-
 with urllib.request.urlopen(sys.argv[1]) as response:
     html = response.read()
     x_request_id = response.getheader('X-Request-Id')
