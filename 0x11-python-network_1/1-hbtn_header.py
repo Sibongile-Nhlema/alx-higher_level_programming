a8086ee31fc81198bac095aeaeb11317276030ac
Sibongile <sibongile.nhlema@gmail.com>
2024-03-03 08:52:40 -0800

    redid task 1 with edge case handling

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-'''takes in url, sends request and displays the value of X-Request_Id variable in the header'''
+'''takes in url, sends request and displays value of X-Request_Id variable in header'''
 
 import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.read()
-    x_request_id = response.getheader('X-Request-Id')
-    print(x_request_id)
+if len(sys.argv) < 2:
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)
+except Exception as e:
+    print(f"Error occurred: {e}")
