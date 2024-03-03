30233f69dbf23074397720c9e3f1289ec8a019fa
Sibongile <sibongile.nhlema@gmail.com>
2024-03-03 02:07:47 -0800

    fixed the indentation errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-	''' Function finds the peak in a list of unordered integers'''
+''' Function finds the peak in a list of unordered integers'''
 
 def find_peak(list_of_integers):
-	''' Function finds the peak in a list of unordered integers'''
-	left = 0, right = len(list_of_integers) - 1
+    ''' Function finds the peak in a list of unordered integers'''
+    left = 0, right = len(list_of_integers) - 1
 
-	while left < right:
-		mid = (left + right) // 2
+    while left < right:
+        mid = (left + right) // 2
 
-		if list_of_integers[mid] < list_of_integers[mid + 1]:
-			left = mid + 1
-		else:
-			right = mid
+	if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
 
-	return list_of_integers[left]
+    return list_of_integers[left]
 
