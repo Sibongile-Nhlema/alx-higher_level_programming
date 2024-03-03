cab06f03cc4790e969fd60486410b86bc8af3065
Sibongile <sibongile.nhlema@gmail.com>
2024-03-03 11:20:16 -0800

    modified task 2 for fix task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+takes in a URL and an email,
+sends a POST request to the passed URL
+with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+uses requests and sys
+'''
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response = requests.post(url, data={'email': email})
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Failed to send POST request. Status code: {response.status_code}")
