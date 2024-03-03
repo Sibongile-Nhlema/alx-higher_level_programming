22c09105c1f3b6c7a3ad52277d8fd3ed771faa1f
Sibongile <sibongile.nhlema@gmail.com>
2024-03-03 02:12:51 -0800

    added better documentation for the function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-''' Function finds the peak in a list of unordered integers'''
+'''Module to find the peak in a list of unordered integers'''
 
 def find_peak(list_of_integers):
-    ''' Function finds the peak in a list of unordered integers'''
+    '''Find the peak in a list of unordered integers using binary search algorithm.
+
+    Args:
+        list_of_integers (list): A list of unordered integers.
+
+    Returns:
+        int: The peak element in the list.
+
+    Example:
+        >>> find_peak([1, 3, 20, 4, 1, 0])
+        20
+    '''
     left = 0
     right = len(list_of_integers) - 1
 
     while left < right:
         mid = (left + right) // 2
 
-	if list_of_integers[mid] < list_of_integers[mid + 1]:
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = mid + 1
         else:
             right = mid
 
     return list_of_integers[left]
-
