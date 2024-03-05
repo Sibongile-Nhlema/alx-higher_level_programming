d11f00f9e7e4047781d01260154c00487de24d13
Sibongile <sibongile.nhlema@gmail.com>
2024-03-05 12:48:21 +0200

    completed task 9: made file executable

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    url = 'https://api.github.com/user'
+    url = 'https://api.github.com/Sibongile-Nhlema'
     response = requests.get(url, auth=(username, password))
 
     try:
