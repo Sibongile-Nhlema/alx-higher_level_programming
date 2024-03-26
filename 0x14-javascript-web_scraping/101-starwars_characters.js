5e2a5ddb386f60638f3f3eb27d2ad543123e50e7
root <Sibongile>
2024-03-26 13:05:21 -0700

    completed task 8 with a pycodestyle error unresolved

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+function fetchCharacters (movieId) {
+  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+  request(apiUrl, function (error, response, body) {
+    if (error) {
+      console.error('Error:', error);
+    } else if (response && response.statusCode !== 200) {
+      console.error('Invalid status code:', response.statusCode);
+    } else {
+      const movieData = JSON.parse(body);
+      const charactersUrls = movieData.characters;
+      let counter = 0;
+
+      charactersUrls.forEach(characterUrl => {
+        request(characterUrl, function (error, response, body) {
+          if (error) {
+            console.error('Error:', error);
+          } else if (response && response.statusCode !== 200) {
+            console.error('Invalid status code:', response.statusCode);
+          } else {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+            counter++;
+          }
+        });
+      });
+    }
+  });
+}
+
+if (movieId) {
+  fetchCharacters(movieId);
+} else {
+  console.error('Usage: node 101-starwars_characters.js <Movie-ID>');
+}
