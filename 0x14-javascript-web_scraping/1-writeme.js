30c9125d41bbd7fe6ecf770fc1fa7f1b45a4ed4f
root <Sibongile>
2024-03-26 09:02:10 -0700

    completed task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const message = process.argv[3];
+
+fs.writeFile(filePath, message, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
