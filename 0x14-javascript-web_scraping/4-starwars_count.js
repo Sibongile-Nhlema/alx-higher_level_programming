5212c4dcde7ad64adec8b4bdbaff4e8f330609d5
root <Sibongile>
2024-03-26 11:31:29 -0700

    removed error handling

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -22,10 +22,5 @@ function totalCharacterPresenceInMovie (apiUrl) {
 }
 
 const args = process.argv.slice(2);
-if (args.length !== 1) {
-  console.error('Usage: node 4-starwars_count.js <api_url>');
-  process.exit(1);
-}
-
 const apiUrl = args[0];
 totalCharacterPresenceInMovie(apiUrl);
