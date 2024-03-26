df8187df06ac2faab8b1890620c8b2d40e520770
root <Sibongile>
2024-03-26 11:36:34 -0700

    rewrote the code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,25 +2,39 @@
 
 const request = require('request');
 
-function totalCharacterPresenceInMovie (apiUrl) {
-  const characterId = 18;
-  let count = 0;
+function getTotalCharacterPresenceInMovies(apiUrl) {
+    const characterId = 18;
+    let count = 0;
 
-  request(apiUrl, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const filmsData = JSON.parse(body).results;
-      filmsData.forEach(film => {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-          count++;
-        }
-      });
-      console.log(count);
-    } else {
-      console.error('Error: Cannot to fetch movie data');
+    function fetchPage(url) {
+        request(url, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const filmsData = JSON.parse(body);
+                filmsData.results.forEach(film => {
+                    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+                        count++;
+                    }
+                });
+                if (filmsData.next) {
+                    fetchPage(filmsData.next);
+                } else {
+                    console.log(count);
+                }
+            } else {
+                console.error('Error: Unable to fetch movie data');
+            }
+        });
     }
-  });
+
+    fetchPage(apiUrl);
 }
 
 const args = process.argv.slice(2);
+if (args.length !== 1) {
+    console.error("Usage: node <script.js> <api_url>");
+    process.exit(1);
+}
+
 const apiUrl = args[0];
-totalCharacterPresenceInMovie(apiUrl);
+getTotalCharacterPresenceInMovies(apiUrl);
+
